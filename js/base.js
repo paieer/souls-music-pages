a87d57adef32f0813951056f509ffc3ce47bc35a
paieer_name <paieer@users.noreply.github.com>
2024-09-11 11:47:22 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,5 +1,61 @@
 'use strict';
 
+const musicData = [
+  {
+    backgroundImage: "https://pages.souls-music.com/images/poster-1.jpg",
+    posterUrl: "https://pages.souls-music.com/images/poster-1.jpg",
+    title: "Happy Moments (Master)",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit x Tonion",
+  },
+  {
+    backgroundImage: "https://pages.souls-music.com/images/poster-2.jpg",
+    posterUrl: "https://pages.souls-music.com/images/poster-2.jpg",
+    title: "We Are Going To Be Ok (Master)",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit x Jhove",
+  },
+  {
+    backgroundImage: "https://pages.souls-music.com/images/poster-3.jpg",
+    posterUrl: "https://pages.souls-music.com/images/poster-3.jpg",
+    title: "Winter Meadow",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit x  juniorodeo",
+  },
+  {
+    backgroundImage: "https://pages.souls-music.com/images/poster-4.jpg",
+    posterUrl: "https://pages.souls-music.com/images/poster-4.jpg",
+    title: "From Where We Started",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit",
+  },
+  {
+    backgroundImage: "https://pages.souls-music.com/images/poster-5.jpg",
+    posterUrl: "https://pages.souls-music.com/images/poster-5.jpg",
+    title: "Where I Found You",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit",
+  },
+];
+
+/**
+ * add Aplayer
+ */
+
+const ap = new APlayer({
+	container: document.getElementById('aplayer1'),
+	fixed: false,
+	listFolded: true,
+	mutex: true,
+	preload: false,
+	audio: []
+});
+
 /**
  * add eventListnere on all elements that are passed
  */
@@ -8,352 +64,141 @@ const addEventOnElements = function (elements, eventType, callback) {
 	for (let i = 0, len = elements.length; i < len; i++) {
 	  elements[i].addEventListener(eventType, callback);
 	}
-  }
+}
+
+
+/**
+ * PLAYLIST
+ *
+ * add all music in playlist, from 'musicData'
+ * <span class="material-symbols-rounded">equalizer</span>
+ * <img src="https://pages.souls-music.com/images/icons8-audio-wave.gif">
+ */
+
+const playlist = document.querySelector("[data-music-list]");
+
+const current_page_url = window.location.href;
+var index = current_page_url.lastIndexOf("\/");
+const num_item = current_page_url.substring(index + 1, current_page_url.length) - 1;
+
+for (let i = 0, len = musicData.length; i < len; i++) {
+playlist.innerHTML += `
+<li>
+	<button class="music-item ${i === num_item ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
+	<img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
+		class="img-cover">
+
+	<div class="item-icon">
+		<span class="material-symbols-rounded">equalizer</span>
+	</div>
+	</button>
+</li>
+`;
+}
 
 
 
-  /**
-   * PLAYLIST
-   *
-   * add all music in playlist, from 'musicData'
-   * <span class="material-symbols-rounded">equalizer</span>
-   * <img src="https://pages.souls-music.com/images/icons8-audio-wave.gif">
-   */
+/**
+ * PLAYLIST MODAL SIDEBAR TOGGLE
+ *
+ * show 'playlist' modal sidebar when click on playlist button in top app bar
+ * and hide when click on overlay or any playlist-item
+ */
 
-  const playlist = document.querySelector("[data-music-list]");
+const playlistSideModal = document.querySelector("[data-playlist]");
+const playlistTogglers = document.querySelectorAll("[data-playlist-toggler]");
+const overlay = document.querySelector("[data-overlay]");
 
-  for (let i = 0, len = musicData.length; i < len; i++) {
-	playlist.innerHTML += `
-	<li>
-	  <button class="music-item ${i === 0 ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
-		<img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
-		  class="img-cover">
+const togglePlaylist = function () {
+playlistSideModal.classList.toggle("active");
+overlay.classList.toggle("active");
+document.body.classList.toggle("modalActive");
+}
 
-		<div class="item-icon">
-		  <span class="material-symbols-rounded">equalizer</span>
-		</div>
-	  </button>
-	</li>
-	`;
-  }
+addEventOnElements(playlistTogglers, "click", togglePlaylist);
 
 
 
-  /**
-   * PLAYLIST MODAL SIDEBAR TOGGLE
-   *
-   * show 'playlist' modal sidebar when click on playlist button in top app bar
-   * and hide when click on overlay or any playlist-item
-   */
+/**
+ * PLAYLIST ITEM
+ *
+ * remove active state from last time played music
+ * and add active state in clicked music
+ */
 
-  const playlistSideModal = document.querySelector("[data-playlist]");
-  const playlistTogglers = document.querySelectorAll("[data-playlist-toggler]");
-  const overlay = document.querySelector("[data-overlay]");
+const playlistItems = document.querySelectorAll("[data-playlist-item]");
 
-  const togglePlaylist = function () {
-	playlistSideModal.classList.toggle("active");
-	overlay.classList.toggle("active");
-	document.body.classList.toggle("modalActive");
-  }
+let currentMusic = num_item;
+let lastPlayedMusic = num_item;
 
-  addEventOnElements(playlistTogglers, "click", togglePlaylist);
+const changePlaylistItem = function () {
+playlistItems[lastPlayedMusic].classList.remove("playing");
+playlistItems[currentMusic].classList.add("playing");
+}
 
-
-
-  /**
-   * PLAYLIST ITEM
-   *
-   * remove active state from last time played music
-   * and add active state in clicked music
-   */
-
-  const playlistItems = document.querySelectorAll("[data-playlist-item]");
-
-  let currentMusic = 0;
-  let lastPlayedMusic = 0;
-
-  const changePlaylistItem = function () {
-	playlistItems[lastPlayedMusic].classList.remove("playing");
-	playlistItems[currentMusic].classList.add("playing");
-  }
-
-  addEventOnElements(playlistItems, "click", function () {
-	lastPlayedMusic = currentMusic;
-	currentMusic = Number(this.dataset.playlistItem);
-	changePlaylistItem();
-  });
-
-
-
-  /**
-   * PLAYER
-   *
-   * change all visual information on player, based on current music
-   */
-
-  const playerBanner = document.querySelector("[data-player-banner]");
-  const playerTitle = document.querySelector("[data-title]");
-  const playerAlbum = document.querySelector("[data-album]");
-  const playerYear = document.querySelector("[data-year]");
-  const playerArtist = document.querySelector("[data-artist]");
-
-  const audioSource = new Audio(musicData[currentMusic].musicPath);
-
-  const changePlayerInfo = function () {
-	playerBanner.src = musicData[currentMusic].posterUrl;
-	playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
-	document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
-	playerTitle.textContent = musicData[currentMusic].title;
-	playerAlbum.textContent = musicData[currentMusic].album;
-	playerYear.textContent = musicData[currentMusic].year;
-	playerArtist.textContent = musicData[currentMusic].artist;
-
-	audioSource.src = musicData[currentMusic].musicPath;
-
-	audioSource.addEventListener("loadeddata", updateDuration);
-	playMusic();
-  }
-
-  addEventOnElements(playlistItems, "click", changePlayerInfo);
-
-  /** update player duration */
-  const playerDuration = document.querySelector("[data-duration]");
-  const playerSeekRange = document.querySelector("[data-seek]");
-
-  /** pass seconds and get timcode formate */
-  const getTimecode = function (duration) {
-	const minutes = Math.floor(duration / 60);
-	const seconds = Math.ceil(duration - (minutes * 60));
-	const timecode = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-	return timecode;
-  }
-
-  const updateDuration = function () {
-	playerSeekRange.max = Math.ceil(audioSource.duration);
-	playerDuration.textContent = getTimecode(Number(playerSeekRange.max));
-  }
-
-  audioSource.addEventListener("loadeddata", updateDuration);
-
-
-
-  /**
-   * PLAY MUSIC
-   *
-   * play and pause music when click on play button
-   */
-
-  const playBtn = document.querySelector("[data-play-btn]");
-
-  let playInterval;
-
-  const playMusic = function () {
-	ap.toggle();
-  }
-
-  playBtn.addEventListener("click", playMusic);
-  ap.on('pause', function () {
-	console.log('player pause');
-	playBtn.classList.remove("active");
-    
-  });
-  ap.on('playing', function () {
-    	console.log('player playing');
-	playBtn.classList.add("active");
-  });
-
-  /** update running time while playing music */
-
-  const playerRunningTime = document.querySelector("[data-running-time");
-
-  const updateRunningTime = function () {
-	playerSeekRange.value = audioSource.currentTime;
-	playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-
-	updateRangeFill();
-	isMusicEnd();
-  }
-
-
-
-  /**
-   * RANGE FILL WIDTH
-   *
-   * change 'rangeFill' width, while changing range value
-   */
-
-  const ranges = document.querySelectorAll("[data-range]");
-  const rangeFill = document.querySelector("[data-range-fill]");
-
-  const updateRangeFill = function () {
-	let element = this || ranges[0];
-
-	const rangeValue = (element.value / element.max) * 100;
-	element.nextElementSibling.style.width = `${rangeValue}%`;
-  }
-
-  addEventOnElements(ranges, "input", updateRangeFill);
-
-
-
-  /**
-   * SEEK MUSIC
-   *
-   * seek music while changing player seek range
-   */
-
-  const seek = function () {
-	audioSource.currentTime = playerSeekRange.value;
-	playerRunningTime.textContent = getTimecode(playerSeekRange.value);
-  }
-
-  playerSeekRange.addEventListener("input", seek);
-
-
-
-  /**
-   * END MUSIC
-   */
-
-  const isMusicEnd = function () {
-	if (audioSource.ended) {
-	  playBtn.classList.remove("active");
-	  audioSource.currentTime = 0;
-	  playerSeekRange.value = audioSource.currentTime;
-	  playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-	  updateRangeFill();
+const loadAplayerMusic = function () {
+	switch (currentMusic+1) {
+		case 1:
+			ap.list.add([
+				{
+					name: '半岛铁盒',
+					author: '周杰伦',
+					url: 'https://echeverra.cn/wp-content/uploads/2022/05/周杰伦-半岛铁盒.mp3',
+					pic: 'https://echeverra.cn/wp-content/uploads/2022/05/周杰伦-半岛铁盒-mp3-image.png'
+				},
+				{
+					name: '给我一首歌的时间',
+					author: '周杰伦',
+					url: 'https://echeverra.cn/wp-content/uploads/2021/06/周杰伦-给我一首歌的时间.mp3',
+					pic: 'https://echeverra.cn/wp-content/uploads/2021/06/周杰伦-给我一首歌的时间-mp3-image.png'
+				}
+			]);
+		case 2:
+			ap.list.add([
+				{
+					name: '时间',
+					author: 'Soul\'s whisper',
+					url: 'https://r2.souls-music.com/music/music-2.mp3',
+					pic: 'https://pages.souls-music.com/images/poster-2.jpg'
+				}
+			]);
 	}
-  }
+
+}
+
+addEventOnElements(playlistItems, "click", function () {
+lastPlayedMusic = currentMusic;
+currentMusic = Number(this.dataset.playlistItem);
+changePlaylistItem();
+// 加载专辑到 aplayer
+ap.list.clear();
+loadAplayerMusic();
+});
+loadAplayerMusic();
 
 
+/**
+ * PLAYER
+ *
+ * change all visual information on player, based on current music
+ */
 
-  /**
-   * SKIP TO NEXT MUSIC
-   */
-
-  const playerSkipNextBtn = document.querySelector("[data-skip-next]");
-
-  const skipNext = function () {
-	lastPlayedMusic = currentMusic;
-
-	// if (isShuffled) {
-	//   shuffleMusic();
-	// } else {
-	//   currentMusic >= musicData.length - 1 ? currentMusic = 0 : currentMusic++;
-	// }
-	ap.skipForward()
-	changePlayerInfo();
-	changePlaylistItem();
-  }
-
-  playerSkipNextBtn.addEventListener("click", skipNext);
+const playerBanner = document.querySelector("[data-player-banner]");
+const playerTitle = document.querySelector("[data-title]");
+const playerAlbum = document.querySelector("[data-album]");
+const playerYear = document.querySelector("[data-year]");
+const playerArtist = document.querySelector("[data-artist]");
 
 
+const changePlayerInfo = function () {
+playerBanner.src = musicData[currentMusic].posterUrl;
+playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
+document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
+playerTitle.textContent = musicData[currentMusic].title;
+playerAlbum.textContent = musicData[currentMusic].album;
+playerYear.textContent = musicData[currentMusic].year;
+playerArtist.textContent = musicData[currentMusic].artist;
 
-  /**
-   * SKIP TO PREVIOUS MUSIC
-   */
-
-  const playerSkipPrevBtn = document.querySelector("[data-skip-prev]");
-
-  const skipPrev = function () {
-	lastPlayedMusic = currentMusic;
-
-	// if (isShuffled) {
-	//   shuffleMusic();
-	// } else {
-	//   currentMusic <= 0 ? currentMusic = musicData.length - 1 : currentMusic--;
-	// }
-	ap.skipBack();
-	changePlayerInfo();
-	changePlaylistItem();
-  }
-
-  playerSkipPrevBtn.addEventListener("click", skipPrev);
-
-
-
-  /**
-   * SHUFFLE MUSIC
-   */
-
-  /** get random number for shuffle */
-  const getRandomMusic = () => Math.floor(Math.random() * musicData.length);
-
-  const shuffleMusic = () => currentMusic = getRandomMusic();
-
-  const playerShuffleBtn = document.querySelector("[data-shuffle]");
-  let isShuffled = false;
-
-  const shuffle = function () {
-	if (isShuffled){
-		playerShuffleBtn.classList.remove("active");
-	} else {
-		playerShuffleBtn.classList.toggle("active");
-	}
-	isShuffled = isShuffled ? false : true;
-  }
-
-  playerShuffleBtn.addEventListener("click", shuffle);
-
-
-
-  /**
-   * REPEAT MUSIC
-   */
-
-  const playerRepeatBtn = document.querySelector("[data-repeat]");
-
-  const repeat = function () {
-	if (!audioSource.loop) {
-	  audioSource.loop = true;
-	  this.classList.add("active");
-	} else {
-	  audioSource.loop = false;
-	  this.classList.remove("active");
-	}
-  }
-
-  playerRepeatBtn.addEventListener("click", repeat);
-
-
-
-  /**
-   * MUSIC VOLUME
-   *
-   * increase or decrease music volume when change the volume range
-   */
-
-  const playerVolumeRange = document.querySelector("[data-volume]");
-  const playerVolumeBtn = document.querySelector("[data-volume-btn]");
-
-  const changeVolume = function () {
-	audioSource.volume = playerVolumeRange.value;
-	audioSource.muted = false;
-
-	if (audioSource.volume <= 0.1) {
-	  playerVolumeBtn.children[0].textContent = "volume_mute";
-	} else if (audioSource.volume <= 0.5) {
-	  playerVolumeBtn.children[0].textContent = "volume_down";
-	} else {
-	  playerVolumeBtn.children[0].textContent = "volume_up";
-	}
-  }
-
-  playerVolumeRange.addEventListener("input", changeVolume);
-
-
-  /**
-   * MUTE MUSIC
-   */
-
-  const muteVolume = function () {
-	if (!audioSource.muted) {
-	  audioSource.muted = true;
-	  playerVolumeBtn.children[0].textContent = "volume_off";
-	} else {
-	  changeVolume();
-	}
-  }
-
-  playerVolumeBtn.addEventListener("click", muteVolume);
+}
+changePlayerInfo();
+addEventOnElements(playlistItems, "click", changePlayerInfo);
